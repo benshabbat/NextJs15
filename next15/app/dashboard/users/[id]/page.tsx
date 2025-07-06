c8dca0b0638568a7f32b14d4a0f93a9a4d7ce1a4
benshabbat <benshabbat27@gmail.com>
2025-07-06 20:10:36 +0300

    add user details and user list pages

---
+++ next15/app/dashboard/users/[id]/page.tsx
@@ -0,0 +1,13 @@
+const page = () => {
+  return (
+    <div>
+      <h1>User Details</h1>
+      <p>This is the user details page.</p>
+      <p>Here you can view and manage user information.</p>
+      <p>Feel free to customize this page as needed.</p>
+      <p>Remember to handle user data securely and responsibly.</p>
+    </div>
+  );
+};
+
+export default page;
