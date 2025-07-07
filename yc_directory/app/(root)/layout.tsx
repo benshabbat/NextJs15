b73ce05cba8d97b14ee03b53ddd95cd035e73234
benshabbat <benshabbat27@gmail.com>
2025-07-07 14:31:06 +0300

    feat: add Layout component and Navbar to structure the application

---
+++ yc_directory/app/(root)/layout.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from "react";
+import Navbar from "../components/Navbar";
+
+type LayoutProps = {
+    readonly children: ReactNode;
+};
+
+export default function Layout({ children }: LayoutProps) {
+    return (
+        <div>
+            <Navbar/>
+            {children}
+        </div>
+    );
+}
